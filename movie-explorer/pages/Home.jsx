dc4529209726de61fa9432df5fc5f0712449e353
aishraq16 <ishraq@Ishraqs-MacBook-Pro.local>
2026-02-21 03:54:43 +0600

    written code for displaying movie cards and searching for movies i Please enter the commit message for your changes. Lines starting

---
+++ movie-explorer/pages/Home.jsx
@@ -0,0 +1,35 @@
+import MovieCard from "../src/components/MovieCard"
+import { useState } from "react"
+
+function Home(){
+    const [searchQuery, setSearchQuery] = useState("")
+    
+    const handleSearch = () => {
+        alert(searchQuery)
+    } 
+
+    const handleChange = (e) => {
+        e.preventDefault() //so the search string does not disappear from the searc box
+        setSearchQuery(e.target.value)
+    }
+
+    const movies = [
+        {id: 1, title: "The Godfather", release_date: "1972"},
+        {id: 2, title: "The Shawshank Redemption", release_date: "1994"},
+        {id: 3, title: "Inglourious Basterds", release_date: "2009"}
+    ]
+
+    return (
+        <div className="home">
+            <form className="search-form" onSubmit={handleSearch}>
+                <input type="text" className="input-search" value={searchQuery} onChange={handleChange} placeholder="Search For A Movie..." />
+                <button type="submit" className="search-button">Search</button>
+            </form>
+            <div className="movie-grid">
+                {movies.map( (movie) => (movie.title.toLowerCase().startsWith(searchQuery) && <MovieCard movie={movie} key={movie.id}/>))}
+            </div>
+        </div>
+    )
+}
+
+export default Home;
---
+++ movie-explorer/src/components/MovieCard.jsx
@@ -0,0 +1,23 @@
+function MovieCard({movie}) {
+
+    const onFavouriteClick = () => {
+        alert("clicked!")
+    }
+
+    return (
+        <div className="movie-poster">
+            <img src={movie.url} alt={movie.title}/>
+            <div className="movie-overlay">
+                <button className="favorite-btn" onClick={onFavouriteClick}>
+                    ♡
+                </button>
+            </div>
+            <div className="movie-info">
+                <h3>{movie.title}</h3>
+                <p>{movie.release_date}</p>
+            </div>
+        </div>
+    )
+}
+
+export default MovieCard;
