630ad946cc5416dd7c059c944f509fec101b37c2
aishraq16 <ishraq@Ishraqs-MacBook-Pro.local>
2026-02-21 04:19:43 +0600

    added navbar and the favourites page

---
+++ movie-explorer/pages/Favourites.jsx
@@ -0,0 +1,5 @@
+function Favourites(){
+    return <h1>No Favourites</h1>
+}
+
+export default Favourites
--- movie-explorer/src/App.jsx
+++ movie-explorer/src/App.jsx
@@ -4,9 +4,22 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import MovieCard from './components/MovieCard'
 import Home from '../pages/Home'
+import { Routes, Route } from 'react-router-dom'
+import Favourites from '../pages/Favourites'
+import NavBar from "./components/NavBar"
 
 function App() {
-  return <Home/>
+  return (
+    <>
+    <NavBar/>
+    <main className='main-contect'>
+      <Routes>
+        <Route path="/" element = {<Home/>}/>
+        <Route path="/favourites" element={<Favourites/>}/>
+      </Routes>
+    </main>
+    </>
+  )
 }
 
 
