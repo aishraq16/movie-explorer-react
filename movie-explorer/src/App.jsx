ef61d0fc0c236d5b4888c6993bce376c3d5ec0f3
aishraq16 <ishraq@Ishraqs-MacBook-Pro.local>
2026-04-07 06:18:02 +0800

    changed css styles to make it visually look better

--- movie-explorer/src/App.jsx
+++ movie-explorer/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import MovieCard from './components/MovieCard'
 import Home from './pages/Home'
 import { Routes, Route } from 'react-router-dom'
